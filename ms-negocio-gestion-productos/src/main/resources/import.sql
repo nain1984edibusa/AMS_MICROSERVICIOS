0af3828be481974a23f376e451ecf6ad7255f31d
Eduardo Bustillos <edibusa_1991@hotmail.com>
2026-07-09 12:26:57 -0500

    ms-gestion pedidos - api composite

--- ms-negocio-gestion-productos/src/main/resources/import.sql
+++ ms-negocio-gestion-productos/src/main/resources/import.sql
@@ -1,6 +1,6 @@
-INSERT INTO tbl_producto (nombre,precio,stock,estado) VALUES ('LAPTOP  ASUS VivoBook Pro',3500,4,'1');
-INSERT INTO tbl_producto (nombre,precio,stock,estado) VALUES ('ALCOHOL ISOPROPILICO 70',7.00,15,'1');
-INSERT INTO tbl_producto (nombre,precio,stock,estado) VALUES ('LAPTOP  ASUS VivoBook Pro',4500,15,'1');
-INSERT INTO tbl_producto (nombre,precio,stock,estado) VALUES ('ALCOHOL ISOPROPILICO 90',7.00,12,'1');
-INSERT INTO tbl_producto (nombre,precio,stock,estado) VALUES ('CARGADOR LENOVO ORIGINAL Modelo 2471',8.00,10,'1');
-INSERT INTO tbl_producto (nombre,precio,stock,estado) VALUES ('LAPTOP  ASUS VivoBook Plus',3800,10,'1');
+INSERT INTO tbl_producto (nombre,precio,stock,estado) VALUES ('DELL 15 DC15250',3500,4,'1');
+INSERT INTO tbl_producto (nombre,precio,stock,estado) VALUES ('Apple MacBook Neo 2026',7.00,15,'1');
+INSERT INTO tbl_producto (nombre,precio,stock,estado) VALUES ('ASUS VivoBook 15 X1504V ',4500,15,'1');
+INSERT INTO tbl_producto (nombre,precio,stock,estado) VALUES ('HP 15-fd0276la',7.00,12,'1');
+INSERT INTO tbl_producto (nombre,precio,stock,estado) VALUES ('LENOVO IdeaPad Slim 3 15IRH10',8.00,10,'1');
+INSERT INTO tbl_producto (nombre,precio,stock,estado) VALUES ('CPU Apple Mac Mini Desktop ',3800,10,'1');
