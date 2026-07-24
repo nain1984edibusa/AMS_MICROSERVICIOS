00eca8bc68d1cb227d4509313cf38b13eb81503c
Eduardo Bustillos <edibusa_1991@hotmail.com>
2026-07-23 20:03:39 -0500

    dockerizar  productos

--- ms-negocio-gestion-productos/src/main/java/com/ebustillos/ms/negocio/gestionproductos/MsNegocioGestionProductosApplication.java
+++ ms-negocio-gestion-productos/src/main/java/com/ebustillos/ms/negocio/gestionproductos/MsNegocioGestionProductosApplication.java
@@ -2,10 +2,8 @@ package com.ebustillos.ms.negocio.gestionproductos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-//@EnableEurekaClient
 @EnableDiscoveryClient
 @SpringBootApplication
 public class MsNegocioGestionProductosApplication {
